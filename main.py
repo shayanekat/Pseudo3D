707caf83c2dad9c5f14cf9395077227406e2fcc3
Shaykat <shaykatfsx@gmail.com>
2021-01-08 17:31:26 +0300

    Init all parameters

--- main.py
+++ main.py
@@ -14,6 +14,43 @@ import pygame
 # =========================BACKEND=========================
 # parameters
 WindowSize = 500
+MapSizeBlock = 20
+WalkDistance = 25
+
+# init
+Walls = []
+PlayerPos = []
+player = ""
+Exit = []
+
+# map design
+rdc = ["WWWWWWWWWWWWWWWWWWWW",
+       "W   W              W",
+       "W W W WWWWWWWWWWWW W",
+       "W W W W            W",
+       "W W   W W WWWWWWWWWW",
+       "W WWWWW W W        W",
+       "W W     W   WWWWWW W",
+       "W W WWWWWWWWW    W W",
+       "W W           WW W W",
+       "W WWWWWWWWWWWWWW W W",
+       "W        P   W   W W",
+       "W WWWWWWWWWW W WWW W",
+       "W          W W   W W",
+       "W WWWWWWWW W WWWWW W",
+       "W W        W W   W W",
+       "W W  WWWWWWW W W W W",
+       "W WW W     W W W   W",
+       "W  WWW W WWW W WWWWW",
+       "WW     W     W     S",
+       "WWWWWWWWWWWWWWWWWWWW"]
+
+# functions
+def hauteur(distance):
+    """
+    Fonction qui donne la hauteur des objets en fonction de la distance
+    """
+    pass
 
 
 # =========================FRONTEND=========================
